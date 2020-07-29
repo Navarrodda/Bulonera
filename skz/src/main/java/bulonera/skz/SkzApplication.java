77f39bf963dd970dea9dddab12e053f22c2f771e
Navarrodda <daviddd.dn@gmail.com>
2020-07-29 11:35:57 -0300

    Create initial commit

---
+++ skz/src/main/java/bulonera/skz/SkzApplication.java
@@ -0,0 +1,13 @@
+package bulonera.skz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkzApplication.class, args);
+	}
+
+}
