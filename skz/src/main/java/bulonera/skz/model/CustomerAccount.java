ef131b53b29ad7d6d0040348d8d17b43d6035d32
Navarrodda <daviddd.dn@gmail.com>
2020-07-31 18:59:33 -0300

    End model

--- skz/src/main/java/bulonera/skz/model/CustomerAccount.java
+++ skz/src/main/java/bulonera/skz/model/CustomerAccount.java
@@ -1,4 +1,34 @@
 package bulonera.skz.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "customer_account")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class CustomerAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "date_account")
+    private Date date;
+
+    @Column(name = "balance_you_owe")
+    private boolean balanceYouOwe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="id_bill")
+    private  Bill bill;
 }
